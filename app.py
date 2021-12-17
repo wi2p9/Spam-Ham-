1a7663e201355ffbd0439e5630ed3af7c878a3f0
ameenmanna8824 <52666434+ameenmanna8824@users.noreply.github.com>
2021-12-17 18:49:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,5 +4,5 @@ model = joblib.load('Email_Class')
 st.title('Spam Ham Classifier')
 ip = st.text_input('Enter your message')       
 op = model.predict([ip])
-if st.button('Predict'):
+if st.button('Predict'):   
   st.title(op[0]) 
